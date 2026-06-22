6ebc8074e142b525ed679d9d0761a14c07032700
gene2404 <ezequielvladimirm@gmail.com>
2026-06-22 12:19:55 -0600

    Add files via upload

---
+++ Proyecto_Web/script.js
@@ -0,0 +1,7 @@
+fuction toggleMenu() {
+    document.getElementById("menu").classList.toggle("active");
+}
+
+function cerrarMenu() {
+    document.getElementById("menu").classList.remove("active");
+}
